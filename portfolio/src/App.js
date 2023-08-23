e89e4aa6da3fed36c4d5410db9d9d1c1d490b010
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-08-23 21:14:11 +0530

    Started

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import About from './components/About';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 >AMIT KUMAR - PORTFOLIO</h1>
+      </header>
+      {/* <About /> */}
+    </div>
+  );
+}
+
+export default App;
