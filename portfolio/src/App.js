19bec8612f2e71064a921defac26e31704dbe555
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-08-27 23:29:43 +0530

    change

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -9,8 +9,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Home</h1>
+
         <h1>Contact</h1>
+
         <h1>Projects</h1>
+        
         <h1>About Me</h1>
       </header>
       <About />
