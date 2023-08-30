0c44fd4163aca3c51999dfd98e28b18f62d58a62
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-08-30 20:41:14 +0530

    Update contact.jsx

--- portfolio/src/components/contact.jsx
+++ portfolio/src/components/contact.jsx
@@ -6,13 +6,6 @@ const ContactMe = () => {
     window.location.href = 'mailto:your.email@example.com?subject=Contact%20From%20Website';
   };
 
-  return (
-    <div className="contact-container">
-      <h1 className='info'>Contact Me</h1>
-      <p className=''>If you have any questions or inquiries, feel free to send me an email.</p>
-      <button className="mail-button" onClick={handleMailClick}>Send Email</button>
-    </div>
-  );
-};
+  
 
 export default ContactMe;
